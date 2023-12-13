6fedfdf99f0f119a869882021bfca051433bd5ef
Thiago Meireles <thiago22md@gmail.com>
2023-12-13 12:12:28 -0300

    function_prev_next_months

--- script.js
+++ script.js
@@ -97,3 +97,30 @@ function initCalendar() {
   daysContainer.innerHTML = days;
   addListner();
 }
+
+function prevMonth() {
+  month--;
+  if (month < 0) {
+    month = 11;
+    year--;
+  }
+  initCalendar();
+}
+
+function nextMonth() {
+  month++;
+  if (month > 11) {
+    month = 0;
+    year++;
+  }
+  initCalendar();
+}
+
+prev.addEventListener("click", prevMonth);
+next.addEventListener("click", nextMonth);
+
+initCalendar();
+
+function addListner() {
+  
+}
