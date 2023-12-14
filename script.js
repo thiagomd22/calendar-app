09e42bcfc67d515929a020e4fb1ea2328dbf6615
Thiago Meireles <thiago22md@gmail.com>
2023-12-14 14:11:45 -0300

    function_gotoDate

--- script.js
+++ script.js
@@ -183,3 +183,18 @@ dateInput.addEventListener("input", () => {
 });
 
 gotoBtn.addEventListener("click", gotoDate);
+
+function gotoDate() {
+  console.log("herer");
+  const dateArr = dateInput.value.split("/");
+  if (dateArr.length === 2) {
+    if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
+      month = dateArr[0] - 1;
+      year = dateArr[1];
+      initCalendar();
+      return;
+    }
+  }
+  alert("Invalid Date");
+}
+
